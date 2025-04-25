4395c9d57f6717053862e7bf6f4f11bc9f2caded
Your Name <your.email@example.com>
2025-04-25 20:50:58 +0700

    Add project files and update repository

---
+++ back-end/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './database/db.js';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+    }
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+});
--- back-end/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const dt = require('./myfirstmodule.js ');
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('The date and time are currently: ' + dt.myDateTime());
-    res.end('Hello World\n');
-}).listen(8000)
